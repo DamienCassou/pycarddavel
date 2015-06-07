ce8e1e1c5234071e94ff8cf89c11535d90e4da38
Damien Cassou <damien@cassou.me>
2015-06-07 13:35:15 +0200

    Initial version

--- pycarddavel.el
+++ pycarddavel.el
@@ -31,9 +31,69 @@
 ;; the process).
 ;;
 ;;; Code:
+
 
+(require 'cl-lib)
+(require 'eieio)
+(require 'helm)
 
+(defun pycarddavel-get-contacts-buffer ()
+  "Put all pycarddav contacts in the returned buffer."
+  (let ((buffer (get-buffer-create "*pycarddavel-contacts*")))
+    (with-current-buffer buffer
+      (erase-buffer)
+      (call-process
+       "pc_query"
+       nil ;; input file
+       (list buffer nil) ;; output to buffer, discard error
+       nil ;; don't redisplay
+       "-m") ;; 1st arg to pc_query: prints email addresses
+      (goto-char (point-min))
+      (kill-whole-line 1))
+    buffer))
+
+(defun pycarddavel-get-contact-from-line (line)
+  "Return a carddav contact read from LINE.
+
+The line must start with something like:
+some@email.com	Some Name
+
+The returned contact is of the form
+ (:name \"Some Name\" :mail \"some@email.com\")"
+  (when (string-match "\\(.*?\\)\t\\(.*?\\)\t" line)
+    (list :name (match-string 2 line) :mail (match-string 1 line))))
+
+(defun pycarddavel--helm-source-init ()
+  "Initialize helm candidate buffer."
+  (helm-candidate-buffer (pycarddavel-get-contacts-buffer)))
+
+(defun pycarddavel--helm-source-select-action (candidate)
+  "Print selected contacts as comma-separated text.
+CANDIDATE is ignored."
+  (ignore candidate)
+  (cl-loop for candidate in (helm-marked-candidates)
+           do (let ((contact (pycarddavel-get-contact-from-line candidate)))
+                (insert (format "\"%s\" <%s>, "
+                                (plist-get contact :name)
+                                (plist-get contact :mail))))))
+
+(defclass pycarddavel--helm-source (helm-source-in-buffer)
+  ((init :initform #'pycarddavel--helm-source-init)
+   (nohighlight :initform t)
+   (action :initform (helm-make-actions
+                      "Select" #'pycarddavel--helm-source-select-action))
+   (requires-pattern :initform 0)))
+
+;;;#autoload
+(defun pycarddavel-search-with-helm ()
+  "Start helm to select your contacts from a list."
+  (interactive)
+  (helm
+   :prompt "contacts: "
+   :sources (helm-make-source "Contacts" 'pycarddavel--helm-source)))
 
 (provide 'pycarddavel)
 
 ;;; pycarddavel.el ends here
+
+;;  LocalWords:  pycarddav carddav
